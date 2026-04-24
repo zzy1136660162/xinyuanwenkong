6f5fadf2553bcb601570787d0622a5248a991896
zhangziyang <1136660162@qq.com>
2026-04-24 09:15:25 +0800

    msg

---
+++ 工具包/admin-ui/src/views/component_example/select/index.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import Demo1 from './_demo1.vue'
+import Demo2 from './_demo2.vue'
+import Demo3 from './_demo3.vue'
+import Demo4 from './_demo4.vue'
+</script>
+
+<template>
+  <div>
+    <FaPageHeader title="选择器" description="FaSelect" />
+    <FaPageMain main-class="p-0">
+      <div class="p-4">
+        <Demo1 />
+      </div>
+    </FaPageMain>
+    <FaPageMain title="组" main-class="p-0">
+      <div class="p-4">
+        <Demo2 />
+      </div>
+    </FaPageMain>
+    <FaPageMain title="多选" main-class="p-0">
+      <div class="p-4">
+        <Demo3 />
+      </div>
+    </FaPageMain>
+    <FaPageMain title="在 FaModal 里展示" main-class="p-0">
+      <div class="p-4">
+        <Demo4 />
+      </div>
+    </FaPageMain>
+  </div>
+</template>
